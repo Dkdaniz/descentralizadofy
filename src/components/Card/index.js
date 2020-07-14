100c9b54d5b4750799555321b49bc5fa645df42c
dkdaniz <daniel@descentralizado.com.br>
2020-07-14 16:42:56 -0300

    [ADD] Card

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <>
+      <Container>
+        <header>
+          <Label color="#7159c1" />
+        </header>
+        <p>Fazer migração completa de Servidor</p>
+        <img src="https://avatars0.githubusercontent.com/u/17903007" alt="" />;
+      </Container>
+    </>
+  );
+};
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-import { Container } from './styles';
+
 import { MdAdd } from 'react-icons/md'
+
+import Card from '../Card';
+
+import { Container } from './styles';
+
 
 export default function List() {
   return (
@@ -13,6 +18,14 @@ export default function List() {
             <MdAdd size={24} color="#FFF" />
           </button>
         </header>
+
+        <ul>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </ul>
       </Container>
     </>
   );
