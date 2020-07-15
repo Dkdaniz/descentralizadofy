ff660ece3f9b7827badf0c5da53a0578cff1ffc1
dkdaniz <daniel@descentralizado.com.br>
2020-07-15 13:03:50 -0300

    [ADD]  finished drop n drop

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -4,10 +4,10 @@ export const Container = styled.div`
   padding: 0 15px;
   height: 100%;
   flex: 0 0 320px;
-  opacity: ${(props) => props.done ? 0.6 : 1};
+  opacity: ${(props) => (props.done ? 0.6 : 1)};
 
   & + div {
-    border-left: 1px solid rgba(0, 0, 0, 0.05)
+    border-left: 1px solid rgba(0, 0, 0, 0.05);
   }
 
   header {
@@ -15,13 +15,11 @@ export const Container = styled.div`
     justify-content: space-between;
     align-items: center;
     height: 42px;
-
     h2 {
       font-weight: 500;
       font-size: 16px;
       padding: 0 10px;
     }
-
     button {
       width: 42px;
       height: 42px;
@@ -31,7 +29,7 @@ export const Container = styled.div`
       cursor: pointer;
     }
   }
-
+  
   ul {
     margin-top: 30px;
   }
